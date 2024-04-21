67e3557a9bdc9229dbe867a04eabf330432419e9
faultypointer <faultypointer@proton.me>
2024-04-21 16:10:35 +0545

    firefox with homemanager

--- system/configuration.nix
+++ system/configuration.nix
@@ -87,8 +87,6 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes"];
 
-  # Install firefox.
-  programs.firefox.enable = true;
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;

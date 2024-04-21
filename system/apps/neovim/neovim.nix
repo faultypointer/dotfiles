b83ddb5176adabef8875dd6d9d74aa69f844084d
faultypointer <faultypointer@proton.me>
2024-04-21 08:55:50 +0545

    neovim colorscheme to gruvbox

--- system/apps/neovim/neovim.nix
+++ system/apps/neovim/neovim.nix
@@ -30,8 +30,34 @@
 	clipboard.register = "unnamedplus";
 	globals.mapleader = " ";
 
-	colorschemes.base16 = {
+	colorschemes.gruvbox = {
 	    enable = true;
+	    settings = {
+		undercurl = true;
+		underline = true;
+		bold = true;
+		italic = {
+		    strings = true;
+		    emphasis = true;
+		    comments = true;
+		    operators = false;
+		    folds = true;
+		};
+		strikethrough = true;
+		invert_selection = false;
+		invert_signs = false;
+		invert_tabline = false;
+		invert_intend_guides = false;
+		inverse = true; #- invert background for search, diffs, statuslines and errors
+		palette_overrides = {};
+		overrides = {};
+		dim_inactive = false;
+		transparent_mode = true;
+	    };
+	};
+
+	colorschemes.base16 = {
+	    enable = false;
 	    # colorscheme = "gruvbox-dark-hard";
 	    colorscheme = "gruvbox-light-soft";
 	 #    customColorScheme = {

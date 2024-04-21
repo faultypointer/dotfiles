cef95720d2bc14ab7a7162edb3b41a4931fd2ea1
faultypointer <faultypointer@proton.me>
2024-04-21 22:19:53 +0545

    firefox

--- system/flake.nix
+++ system/flake.nix
@@ -14,6 +14,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    firefox-addons = {
+	url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+	inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     nix-colors.url = "github:misterio77/nix-colors";
   };
 

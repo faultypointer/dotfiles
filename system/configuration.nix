3e68a723b654f1fa34f8779f70f9adb85c79fa38
faultypointer <faultypointer@proton.me>
2024-05-13 12:06:57 +0545

    added dbus

--- system/configuration.nix
+++ system/configuration.nix
@@ -138,11 +138,13 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
- #  programs.gnupg.agent = {
- #    enable = true;
- #    enableSSHSupport = true;
-	# pinentryPackage = pkgs.pinentry-curses;
- #  };
+
+  services.dbus.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+	pinentryPackage = pkgs.pinentry-curses;
+  };
 
   # List services that you want to enable:
   services.fprintd = {

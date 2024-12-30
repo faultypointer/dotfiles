86cbc478eff46f65558107bb0427c41d6233b19e
faultypointer <faultypointer@proton.me>
2024-12-30 12:59:34 +0545

    scripts

--- home/scripts/default.nix
+++ home/scripts/default.nix
@@ -4,6 +4,10 @@ let
   batman = pkgs.writeShellScriptBin "batman" (builtins.readFile ./batman.sh);
 in
 {
+  imports = [
+    ./sound.nix
+    ./brightness.nix
+  ];
   home.packages = with pkgs; [
     skim
     fzf

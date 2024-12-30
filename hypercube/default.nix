4cfe9290d649024748cddf850dd326e81b8349b2
faultypointer <faultypointer@proton.me>
2024-12-30 15:41:11 +0545

    zsh system

--- hypercube/default.nix
+++ hypercube/default.nix
@@ -1,5 +1,4 @@
-{ pkgs, ... }:
-{
+{ pkgs, ... }: {
   imports = [
     ./hardware-configuration.nix
 
@@ -12,17 +11,14 @@
   networking.hostName = "hypercube";
   networking.networkmanager.enable = true;
 
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   time.timeZone = "Asia/Kathmandu";
 
   i18n.defaultLocale = "en_US.UTF-8";
 
   services.flatpak.enable = true;
-  programs.nix-ld.enable = true;
+  programs.zsh.enable = true;
 
   services.xserver.xkb = {
     layout = "us";
@@ -32,18 +28,13 @@
   users.users.faulty = {
     isNormalUser = true;
     description = "faulty pointer";
-    extraGroups = [
-      "networkmanager"
-      "wheel"
-    ];
+    shell = pkgs.zsh;
+    extraGroups = [ "networkmanager" "wheel" ];
   };
 
   nixpkgs.config.allowUnfree = true;
 
-  environment.systemPackages = with pkgs; [
-    neovim
-    git
-  ];
+  environment.systemPackages = with pkgs; [ neovim git ];
 
   system.stateVersion = "24.11";
 

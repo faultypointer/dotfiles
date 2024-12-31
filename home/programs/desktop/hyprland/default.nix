33e29a928b4ebd8068bbe78eca1acf31ddbcb9b1
faultypointer <faultypointer@proton.me>
2024-12-31 09:21:59 +0545

    yazi

--- home/programs/desktop/hyprland/default.nix
+++ home/programs/desktop/hyprland/default.nix
@@ -4,6 +4,7 @@
 
     # other programs
     ../../terminal/kitty
+    ../../terminal/yazi
   ];
 
   wayland.windowManager.hyprland = {
@@ -11,25 +12,23 @@
     settings = {
       xwayland.enabled = true;
       general = {
-	no_border_on_floating = true;
-	gaps_in = 2;
-	gaps_out = 4;
+        no_border_on_floating = true;
+        gaps_in = 2;
+        gaps_out = 4;
       };
 
       decoration = {
         rounding = 2;
-	active_opacity = 0.85;
-	inactive_opacity = 0.75;
-	fullscreen_opacity = 1.0;
+        active_opacity = 0.85;
+        inactive_opacity = 0.75;
+        fullscreen_opacity = 1.0;
       };
 
-      input.touchpad = {
-        natural_scroll = true;
-      };
+      input.touchpad = { natural_scroll = true; };
 
       gestures = {
         workspace_swipe = true;
-	workspace_swipe_fingers = 3;
+        workspace_swipe_fingers = 3;
       };
     };
   };
--- home/programs/terminal/shell/ssh.nix
+++ home/programs/terminal/shell/ssh.nix
@@ -7,34 +7,34 @@
       "github.com" = {
         hostname = "github.com";
         user = "git";
-	identityFile = "/home/${variables.username}/.ssh/private/iac-github";
+        identityFile = "/home/faulty/.ssh/private/faultygithub";
       };
 
-      "github.com-andsnap" = { 
+      "github.com-andsnap" = {
         hostname = "github.com";
         user = "git";
-	identityFile = "/home/${variables.username}/.ssh/private/andsnap";
+        identityFile = "/home/${variables.username}/.ssh/private/andsnap";
       };
 
       "gitlab.com" = {
         hostname = "gitlab.com";
         user = "git";
-	identityFile = "/home/${variables.username}/.ssh/private/scientiac";
+        identityFile = "/home/${variables.username}/.ssh/private/scientiac";
       };
 
       "tilde.team" = {
         hostname = "tilde.team";
-	identityFile = "/home/${variables.username}/.ssh/private/scientiac";
+        identityFile = "/home/${variables.username}/.ssh/private/scientiac";
       };
 
       "cosmic.voyage" = {
         hostname = "cosmic.voyage";
-	identityFile = "/home/${variables.username}/.ssh/private/scientiac";
+        identityFile = "/home/${variables.username}/.ssh/private/scientiac";
       };
 
       "hashbang.sh" = {
         hostname = "de1.hashbang.sh";
-	identityFile = "/home/${variables.username}/.ssh/private/scientiac";
+        identityFile = "/home/${variables.username}/.ssh/private/scientiac";
       };
     };
   };

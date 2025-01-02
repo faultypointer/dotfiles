61679eccb92c399fe302366b0491c2aa442f8d4e
faultypointer <faultypointer@proton.me>
2025-01-02 20:54:49 +0545

    firefox and spicetify app

--- flake.nix
+++ flake.nix
@@ -21,25 +21,37 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    spicetify-nix = {
+      url = "github:Gerg-L/spicetify-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     hyprland.url = "github:hyprwm/Hyprland";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-    nixosConfigurations = {
-      hypercube = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./hypercube
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+    let variables = import ./home/faulty/variables.nix { inherit inputs; };
+    in {
+      nixosConfigurations = {
+        hypercube = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit self inputs variables; };
+          modules = [
+            ./hypercube
 
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.backupFileExtension = "HMbackup";
-            home-manager.extraSpecialArgs = { inherit inputs; };
-            home-manager.users.faulty = import ./home/faulty;
-          }
-        ];
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.backupFileExtension = "HMbackup";
+              home-manager.extraSpecialArgs = { inherit inputs variables; };
+              home-manager.users.faulty = import ./home/faulty;
+            }
+          ];
+        };
       };
     };
-  };
 }
--- home/faulty/default.nix
+++ home/faulty/default.nix
@@ -1,18 +1,24 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   imports = [
     ../programs/desktop/hyprland
     ../programs/terminal/neovim
     ../programs/terminal/shell
+    ../programs/desktop/firefox
+    ../programs/desktop/spicetify.nix
 
     ../scripts
   ];
   home.username = "faulty";
   home.homeDirectory = "/home/faulty";
 
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [ "spotify" ];
+
   home.packages = with pkgs; [
     bat
     fastfetch
     nerd-fonts.fantasque-sans-mono
+    brave
 
     sioyek
     zed-editor

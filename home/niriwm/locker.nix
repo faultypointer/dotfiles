0f151939d6c97f87205d818772bc6650f3bf57d8
faulty <faultypointer@proton.me>
2025-02-21 12:28:46 +0545

    increase timeout

--- home/niriwm/locker.nix
+++ home/niriwm/locker.nix
@@ -5,44 +5,45 @@ let
   systemctl = config.systemd.user.systemctlPath;
 in {
   home.file."./.config/swaylock/config" = {
-    text = /*ini*/''
-      # Size
-      indicator-radius=100
-      indicator-thickness=5
+    text = # ini
+      ''
+        # Size
+        indicator-radius=100
+        indicator-thickness=5
 
-      # Colors
-      inside-color=00000000
-      separator-color=00000000
-      inside-ver-color=00000000
-      inside-clear-color=00000000
-      inside-wrong-color=00000000
-      line-color=00000000
-      line-ver-color=00000000
-      line-wrong-color=00000000
-      line-clear-color=00000000
-      ring-ver-color=00000000
-      ring-wrong-color=FF0000
-      ring-clear-color=${variables.color.text}
-      ring-ver-color=${variables.color.text}
-      bs-hl-color=${variables.color.accent}
-      key-hl-color=${variables.color.text}
-      ring-color=00000000
-      text-color=00000000
-      text-ver-color=00000000
-      text-wrong-color=00000000
-      text-clear-color=00000000
-    '';
+        # Colors
+        inside-color=00000000
+        separator-color=00000000
+        inside-ver-color=00000000
+        inside-clear-color=00000000
+        inside-wrong-color=00000000
+        line-color=00000000
+        line-ver-color=00000000
+        line-wrong-color=00000000
+        line-clear-color=00000000
+        ring-ver-color=00000000
+        ring-wrong-color=FF0000
+        ring-clear-color=${variables.color.text}
+        ring-ver-color=${variables.color.text}
+        bs-hl-color=${variables.color.accent}
+        key-hl-color=${variables.color.text}
+        ring-color=00000000
+        text-color=00000000
+        text-ver-color=00000000
+        text-wrong-color=00000000
+        text-clear-color=00000000
+      '';
   };
 
   services.swayidle = {
     enable = true;
     timeouts = [
       {
-        timeout = 60;
+        timeout = 120;
         command = "${pidof} swaylock && ${systemctl} suspend";
       }
       {
-        timeout = 150;
+        timeout = 180;
         command = "${pidof} swaylock || ${niri} msg action spawn -- blurlock";
       }
       {
@@ -63,7 +64,5 @@ in {
     ];
   };
 
-  systemd.user.services.swayidle.Unit = lib.mkForce {
-    After = "niri.service";
-  };
+  systemd.user.services.swayidle.Unit = lib.mkForce { After = "niri.service"; };
 }

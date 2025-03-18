8e458a20eb03ef344815cdaf233c856f84115af8
faultypointer <faultypointer@proton.me>
2025-03-18 22:47:51 +0545

    nix for fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,2 +1,6 @@
 zoxide init fish | source
 starship init fish | source
+
+if test -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
+    source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
+end

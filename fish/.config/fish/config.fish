374d62c3bfebde1bf6f815e57c9a5d9d65ce9aa9
faultypointer <faultypointer@proton.me>
2025-03-23 21:29:32 +0545

    things not working
    exporing variable in fish idk how to do it
    swayidle to not lock when firefox is playing video

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,3 +4,5 @@ starship init fish | source
 if test -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
     source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
 end
+
+set -gx INFOPATH $INFOPATH /usr/share/info/emacs

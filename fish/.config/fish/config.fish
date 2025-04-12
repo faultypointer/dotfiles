307ff298699ab7e8e70dc850c03864a65b346d23
faultypointer <faultypointer@proton.me>
2025-04-12 20:09:22 +0545

    cleaning up previous stuff

--- fish/.config/fish/config.fish
+++
@@ -1,7 +0,0 @@
-zoxide init fish | source
-starship init fish | source
-
-if test -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
-    source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
-end
-

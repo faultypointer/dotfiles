2b4139a7de80ccebbdc7b64ade48eb734fd8dde4
faultypointer <faultypointer@proton.me>
2026-03-02 22:32:01 +0545

    backup

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -28,9 +28,17 @@ fish_add_path ~/bin
 fish_add_path ~/.local/bin
 fish_add_path ~/.config/herd-lite/bin/
 
-set -Ux EDITOR nvim
+set -Ux EDITOR vim
 
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
 set -gx PATH $HOME/.cabal/bin $PATH /home/faulty/.ghcup/bin # ghcup-env
 
 direnv hook fish | source
+
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+test -r '/home/faulty/.opam/opam-init/init.fish' && source '/home/faulty/.opam/opam-init/init.fish' >/dev/null 2>/dev/null; or true
+# END opam configuration

32ba789fa75562dc2988c286a56c5d1c135a9df7
faultypointer <faultypointer@proton.me>
2026-03-19 22:30:43 +0545

    doomemacs

--- emacs/.emacs.d/faulty-theme.el
+++
@@ -1,9 +0,0 @@
-(deftheme faulty
-  "Created 2026-03-18.")
-
-(custom-theme-set-variables
- 'faulty
- '(connection-local-profile-alist '((vc-git-connection-default-profile (vc-git--program-version))))
- '(connection-local-criteria-alist '(((:application vc-git) vc-git-connection-default-profile))))
-
-(provide-theme 'faulty)

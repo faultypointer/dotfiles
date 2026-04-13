19b06f14b9ea69f2034f4ef70ff2101337e7b311
faultypointer <faultypointer@proton.me>
2026-04-13 17:32:13 +0545

    make new kitty {tab/window/os_window} open with cwd

--- home/programs/kitty/default.nix
+++ home/programs/kitty/default.nix
@@ -2,13 +2,11 @@
   programs.kitty = {
     enable = true;
     keybindings = {
-      # Reset existing mappings
-      "ctrl+alt+tab" = "next_tab";
-      "ctrl+alt+shift+tab" = "previous_tab";
-      # Send Ctrl Tab and Ctrl + Shift + Tab for Tmux navigation
-      "ctrl+tab" = "send_text all \\x1b[27;5;9~";
-      "ctrl+shift+tab" = "send_text all \\x1b[27;6;9~";
-      "alt+tab" = "send_text all \\x1b[27;3;9~";
+      "ctrl+shift+t" = "new_tab_with_cwd";
+      "ctrl+shift+n" = "new_os_window_with_cwd";
+      "ctrl+shift+enter" = "new_window_with_cwd";
+      "ctrl+]" = "resize_window wider";
+      "ctrl+[" = "resize_window narrower";
     };
     settings = {
       scrollback_lines = 10000;
@@ -25,6 +23,8 @@
       cursor_trail = 3;
       cursor_trail_decay = "0.1 0.4";
       window_padding_width = 10;
+
+      enabled_layouts = "splits:split_axis=auto";
     };
   };
 }
